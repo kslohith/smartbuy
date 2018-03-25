f2bb8448c6064f1fd18dfd4bb3262840f5b79e7e
kslohith <kslohith1729@gmail.com>
2018-03-25 06:20:09 +0530

    Initial commit

---
+++ app/src/main/java/codingwithmitch/com/googlemapsgoogleplaces/clothes.java
@@ -0,0 +1,8 @@
+package codingwithmitch.com.googlemapsgoogleplaces;
+
+/**
+ * Created by K.S.LOHITH on 25-03-2018.
+ */
+
+public class clothes {
+}
---
+++ app/src/main/java/codingwithmitch/com/googlemapsgoogleplaces/grains.java
@@ -0,0 +1,15 @@
+package codingwithmitch.com.googlemapsgoogleplaces;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by K.S.LOHITH on 25-03-2018.
+ */
+
+public class grains extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gr);
+    }
+}
